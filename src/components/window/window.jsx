f2fd7e623509c917c503ab0aac085ca84517cf47
vasilich99 <oleg_kovtkov@mail.ru>
2021-09-25 18:31:08 +0300

    fix css, added event listener to button close

--- src/components/window/window.jsx
+++ src/components/window/window.jsx
@@ -1,18 +1,20 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 import css from './window.module.scss';
 
 export default function Window(props) {
+  const [open, setOpen] = useState(props.open);
+
   return (
     <div
       className={classNames(css.container, {
-        [css.container_open]: props.open,
+        [css.container_open]: open,
       })}
     >
       <section className={css.window}>
         <header className={css.header}>
           <span>{props.title}</span>
-          <button aria-label="закрыть" type="button" className={css.close} />
+          <button aria-label="закрыть" type="button" className={css.close} onClick={() => setOpen(false)} />
         </header>
         {props.children}
       </section>
