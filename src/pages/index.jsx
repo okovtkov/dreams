90bab772ae72ee5a69cfa11594213e92aa226dbc
vasilich99 <oleg_kovtkov@mail.ru>
2021-09-28 18:12:54 +0300

    feat: made a component About (layout)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import DreamConstructor from '../components/dream-constructor/dream-constructor';
 import Header from '../components/header/header';
+import About from '../components/about/about';
 
 export default function PageIndex() {
   return (
     <>
       <DreamConstructor />
       <Header />
+      <About />
     </>
   );
 }
