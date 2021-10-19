7b146c8a5dbd358ffb50a5cc7b51f5618ef1fbea
vasilich99 <oleg_kovtkov@mail.ru>
2021-10-19 16:41:32 +0300

    Add CDN

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   // Use the CDN in production and localhost for development.
