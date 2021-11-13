8b79e8c12b72f58b49b1226d5b87b6912d074af2
vasilich99 <oleg_kovtkov@mail.ru>
2021-11-13 21:42:16 +0300

    убрал dangerouslySetInnerHTML, создал dream-preview

---
+++ src/components/dream-preview/dream-preview.jsx
@@ -0,0 +1,58 @@
+import React, { useMemo } from 'react';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import css from './dream-preview.module.scss';
+import DreamCategories, { categories } from '../dream-categories/dream-categories';
+
+export default function DreamPreview(props) {
+  const clickAtDreamHandler = (event, dream) => {
+    event.preventDefault();
+    props.setActiveDreamId(dream.id);
+  };
+
+  const getCategoriesByIds = (ids) => ids.map((id) => {
+    const category = categories.find((one) => one.id === id);
+    if (!category) throw new Error(`Нет категории с таким ID: ${id}`);
+    return category;
+  });
+
+  const id = useMemo(() => (props.dream.type === 'video'
+    ? new URL(props.dream.text).pathname.slice(1) : ''),
+  [props.dream]);
+
+  return (
+    <li
+      key={props.dream.id}
+      className={classNames(css.item, {
+        [css.item_text]: !props.dream.preview,
+      })}
+    >
+      <Link
+        className={css.link}
+        to={`/dream/${props.dream.id}`}
+        onClick={(e) => clickAtDreamHandler(e, props.dream)}
+        onKeyDown={(e) => clickAtDreamHandler(e, props.dream)}
+        tabIndex={0}
+        role="button"
+      >
+        {props.dream.type === 'video' && (
+          <img
+            srcSet={`https://vumbnail.com/${id}_large.jpg 640w,
+            https://vumbnail.com/${id}_medium.jpg 200w,
+            https://vumbnail.com/${id}_small.jpg 100w`}
+            sizes="(max-width: 640px) 100vw, 640px"
+            src={`https://vumbnail.com/${id}.jpg`}
+            alt="превью"
+          />
+        )}
+        {props.dream.type === 'text' && (
+          <p className={css.text}>{props.dream.text}</p>
+        )}
+        <DreamCategories
+          categories={getCategoriesByIds(props.dream.categories)}
+          mode="small"
+        />
+      </Link>
+    </li>
+  );
+}
--- src/components/dreams-list/dreams-list.jsx
+++ src/components/dreams-list/dreams-list.jsx
@@ -1,23 +1,15 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import classNames from 'classnames';
 import DreamReview from '../dream-review/dream-review';
 import dreams from '../../api/dreams';
 import css from './dreams-list.module.scss';
-import DreamCategories, { categories } from '../dream-categories/dream-categories';
 import Positioner from '../positioner/positioner';
+import DreamPreview from '../dream-preview/dream-preview';
 
 export default function DreamsList() {
   const [dreamsList, setDreamsList] = useState([]);
   const [activeDreamId, setActiveDreamId] = useState(null);
-
-  const getCategoriesByIds = (ids) => ids.map((id) => {
-    const category = categories.find((one) => one.id === id);
-    if (!category) throw new Error(`Нет категории с таким ID: ${id}`);
-    return category;
-  });
 
   useEffect(() => {
     if (activeDreamId) {
@@ -26,11 +18,6 @@ export default function DreamsList() {
       history.pushState(null, document.title, '/dreams');
     }
   }, [activeDreamId]);
-
-  const clickAtDreamHandler = (event, dream) => {
-    event.preventDefault();
-    setActiveDreamId(dream.id);
-  };
 
   useEffect(() => {
     dreams.get().then((result) => setDreamsList(result));
@@ -45,36 +32,11 @@ export default function DreamsList() {
         <h2 className={css.title}>Dream ambassadors</h2>
         <ul className={css.list}>
           {dreamsList.map((dream) => (
-            <li
-              key={dream.id}
-              className={classNames(css.item, {
-                [css.item_text]: !dream.preview,
-              })}
-            >
-              <Link
-                className={css.link}
-                to={`/dream/${dream.id}`}
-                onClick={(e) => clickAtDreamHandler(e, dream)}
-                onKeyDown={(e) => clickAtDreamHandler(e, dream)}
-                tabIndex={0}
-                role="button"
-              >
-                {dream.type === 'video' && (
-                  <div
-                    className={css.image}
-                    // eslint-disable-next-line react/no-danger
-                    dangerouslySetInnerHTML={{ __html: dream.preview }}
-                  />
-                )}
-                {dream.type === 'text' && (
-                  <p className={css.text}>{dream.text}</p>
-                )}
-                <DreamCategories
-                  categories={getCategoriesByIds(dream.categories)}
-                  mode="small"
-                />
-              </Link>
-            </li>
+            <DreamPreview
+              dream={dream}
+              activeDreamId={activeDreamId}
+              setActiveDreamId={setActiveDreamId}
+            />
           ))}
         </ul>
       </Positioner>
