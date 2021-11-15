75319518695da7cca7c53cfe7b42a50e6454f7f8
vasilich99 <oleg_kovtkov@mail.ru>
2021-11-15 23:03:45 +0300

    переписал dream-constructor на useReducer

--- src/components/dream-constructor/dream-constructor.jsx
+++ src/components/dream-constructor/dream-constructor.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable one-var */
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import dreams from '../../api/dreams';
 import videos from '../../api/video';
 import css from './dream-constructor.module.scss';
@@ -14,70 +14,79 @@ import DreamVideo from './dream-video';
 import DreamForm from './dream-form';
 import DreamFinished from './dream-finished';
 
+function init(state) {
+  return { ...state };
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'stepUp': return { ...state, step: state.step + 1 };
+    case 'stepDown': return { ...state, step: Math.max(state.step - 1, 1) };
+    case 'type': return { ...state, type: action.newType };
+    case 'categories': return { ...state, categories: action.categories };
+    case 'video': return { ...state, video: action.video };
+    case 'text': return { ...state, text: action.text };
+    case 'name': return { ...state, name: action.name };
+    case 'email': return { ...state, email: action.email };
+    case 'country': return { ...state, country: action.country };
+    case 'reset': return init(action.payload);
+    default: return '';
+  }
+}
+
 function DreamConstructor(props) {
-  const [selectedCategories, setSelectedCategories] = useState([]);
-  const [step, setStep] = useState(1);
-  const [type, setType] = useState('');
-  const [video, setVideo] = useState(null);
-  const [text, setText] = useState('');
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [country, setCountry] = useState('USA');
+  const initialState = {
+    categories: [],
+    step: 1,
+    type: '',
+    video: null,
+    text: '',
+    name: '',
+    email: '',
+    country: 'USA',
+  };
+  const [state, dispatch] = useReducer(reducer, initialState, init);
 
   const title = () => {
-    const titleText = step > 4 ? 'Finished' : `Step ${step}/4`;
+    const titleText = state.step > 4 ? 'Finished' : `Step ${state.step}/4`;
     return (
       <button
         type="button"
         className={classNames(css.title, {
-          [css.titleWithPrev]: step > 1,
+          [css.titleWithPrev]: state.step > 1,
         })}
-        onClick={() => setStep(Math.max(step - 1, 1))}
+        onClick={() => dispatch({ type: 'stepDown' })}
       >
         {titleText}
       </button>
     );
   };
 
-  const nextStep = () => {
-    setStep(step + 1);
-  };
-
   const onRecordVideo = (recorded) => {
-    setVideo(recorded);
-    nextStep();
+    dispatch({ type: 'video', video: recorded });
+    dispatch({ type: 'stepUp' });
   };
 
   const onChangeType = (newType) => {
-    setType(newType);
-    nextStep();
+    dispatch({ type: 'type', newType });
+    dispatch({ type: 'stepUp' });
   };
 
   const toggleCategory = (category) => {
-    const categories = selectedCategories.includes(category)
-      ? selectedCategories.filter((item) => item !== category)
-      : [...selectedCategories, category];
+    const categories = state.categories.includes(category)
+      ? state.categories.filter((item) => item !== category)
+      : [...state.categories, category];
     if (categories.length > 5) return;
-    setSelectedCategories(categories);
-  };
-
-  const resetSteps = () => {
-    setStep(1);
-    setSelectedCategories([]);
-    setText('');
-    setVideo(null);
-    setName('');
-    setEmail('');
-    setCountry('USA');
+    dispatch({ type: 'categories', categories });
   };
 
   const uploadVideo = () => {
-    if (type !== 'video') {
+    if (state.type !== 'video') {
       return Promise.resolve({ url: null });
     }
-    return videos.create(video)
+    return videos.create(state.video)
       .then((response) => {
-        videos.upload(response.upload.upload_link, video);
+        videos.upload(response.upload.upload_link, state.video);
         return response;
       })
       .then((response) => response.link);
@@ -87,57 +96,62 @@ function DreamConstructor(props) {
     event.preventDefault();
     uploadVideo()
       .then((response) => {
-        const videoLink = response.url;
+        const videoLink = response;
         dreams.create({
-          categories: selectedCategories.map((category) => category.id),
-          text: type === 'text' ? text : videoLink,
-          name,
-          email,
-          country,
-          type,
+          categories: state.categories.map((category) => category.id),
+          text: state.type === 'text' ? state.text : videoLink,
+          name: state.name,
+          email: state.email,
+          country: state.country,
+          type: state.type,
         });
       })
-      .then(nextStep);
+      .then(dispatch({ type: 'stepUp' }));
   };
 
   const onClose = () => {
     props.onClose();
-    // resetSteps();
+    dispatch({ type: 'reset', payload: initialState });
   };
 
   return (
     <Window title={title()} open={props.open} onClose={onClose}>
       <form action="#" name="form" onSubmit={onSubmit}>
-        {step === 1 && (
+        {state.step === 1 && (
           <DreamType onChangeType={onChangeType} />
         )}
-        {step === 2 && (
+        {state.step === 2 && (
           <DreamCategory
-            selectedCategories={selectedCategories}
+            selectedCategories={state.categories}
             onToggleCategory={toggleCategory}
-            onClickNextStep={nextStep}
+            onClickNextStep={() => dispatch({ type: 'stepUp' })}
           />
         )}
-        {step === 3 && type === 'text' && (
+        {state.step === 3 && state.type === 'text' && (
           <DreamMessage
-            onClickNextStep={nextStep}
-            onChangeText={setText}
+            onClickNextStep={() => dispatch({ type: 'stepUp' })}
+            onChangeText={(text) => dispatch({ type: 'text', text })}
           />
         )}
-        {step === 3 && type === 'video' && (
+        {state.step === 3 && state.type === 'video' && (
           <DreamVideo
             onClickNextStep={onRecordVideo}
             open={props.open}
           />
         )}
-        {step === 4 && (
+        {state.step === 4 && (
           <DreamForm
-            onChangeName={setName}
-            onChangeEmail={setEmail}
-            onChangeCountry={setCountry}
+            onChangeName={(name) => dispatch({ type: 'name', name })}
+            onChangeEmail={(email) => dispatch({ type: 'email', email })}
+            onChangeCountry={(country) => dispatch({ type: 'country', country })}
           />
         )}
-        {step === 5 && <DreamFinished onClose={props.onClose} resetSteps={resetSteps} />}
+        {state.step === 5 && (
+          <DreamFinished
+            onClose={props.onClose}
+            resetSteps={() => dispatch({ type: 'reset', payload: initialState })}
+          />
+        )}
       </form>
     </Window>
   );
